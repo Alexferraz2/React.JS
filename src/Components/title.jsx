4492787b337aaa32f91f9c2486e902dd064bb3ed
Alexferraz2 <alex_ferraz2@hotmail.com>
2024-04-23 20:41:25 -0300

    Criado primeiro componente react.jsx

---
+++ src/Components/title.jsx
@@ -0,0 +1,10 @@
+export function Title() {
+    return(
+        <div>
+            <h1>
+                Título da página
+            </h1>
+        </div>
+    )
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
+import { Title } from "@/Components/title";
+
 function Home() {
   return (
     <div>
+      <Title/>
       <h1>Olá mundo!!!</h1>
     </div>
   )
