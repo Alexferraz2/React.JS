dd931435a1b0c99171a3ead439261ed9f636eb0e
Alexferraz2 <alex_ferraz2@hotmail.com>
2024-04-29 21:13:47 -0300

    Importado font e configurações do título do site

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,16 @@
+import Head from 'next/head';
+
 import '../src/style.css'
 
 function App({ Component, pageProps}) {
-    return < Component {...pageProps} />
+    return (
+    <>
+    <Head>
+        <title>Minha página</title>
+    </Head>
+        < Component {...pageProps} />
+    </>
+    )
 }
 
 export default App;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,24 @@
+import Head from "next/head";
 import { Title } from "@/Components/title";
 import { SubTitle, NewSubTitle } from "@/Components/subtitle/SubTitle";
 
 
 function Home() {
   return (
-    <div>
-      <Title>Usando o children</Title>
-      <Title text='Usando props'/>
-      <Title texto='Usando propriedade'/>
-      <SubTitle Newtext='Novo texto'/>
-      <NewSubTitle/>
-      <h1>Olá mundo!!!</h1>
-    </div>
+    <>
+      <head>
+        <title>Minha outra página</title>
+      </head>
+      <div>
+        <Title>Usando o children</Title>
+        <Title text='Usando props'/>
+        <Title texto='Usando propriedade'/>
+        <SubTitle Newtext='Novo texto'/>
+        <NewSubTitle/>
+        <h1>Olá mundo!!!!!!!!</h1>
+      </div>
+    </>
+    
   )
 }
 
